a6f053ef46669ec4ca29107def45f6520fed5f9c
David Loeffler <d.loeffler.01@cantab.net>
2025-01-14 10:01:15 +0100

    Examples for lecture 2

--- Charmey/Ideals.lean
+++ Charmey/Ideals.lean
@@ -1,32 +1,41 @@
 import Mathlib
+/-!
+# Examples for Lecture 2: Nilpotents in commutative rings
+-/
 
-variable {R : Type*} [Ring R]
+/-- Example 1: If `a` is nilpotent, then `a * b` is nilpotent for any `b`. -/
+lemma nilpotent_mul (R : Type) [CommRing R] (a b : R) (ha : IsNilpotent a) :
+    IsNilpotent (a * b) := by
+  rw [IsNilpotent] at *
+  obtain ⟨n, hn⟩ := ha
+  use n
+  rw [mul_pow, hn, zero_mul]
 
-/-- If `I` is a prime ideal, and `a ^ n ∈ I` for some `n`, then `a ∈ I`. -/
-lemma mem_primeIdeal_of_pow_mem
-    {I : Ideal R} (hI : Ideal.IsPrime I) {a : R} {n : ℕ} (ha : a ^ n ∈ I) :
+
+
+/-- Helper lemma for example 2: If `I` is a prime ideal, and `a ^ n ∈ I` for some
+`n`, then `a ∈ I`.
+-/
+lemma mem_primeIdeal_of_pow_mem (R : Type) [CommRing R]
+    (I : Ideal R) (hI : Ideal.IsPrime I) (a : R) (n : ℕ) (ha : a ^ n ∈ I) :
     a ∈ I := by
   induction n with
   | zero => -- base case
+      apply Ideal.mem_of_one_mem
       rw [pow_zero] at ha
-      rw [← mul_one a]
-      apply Ideal.mul_mem_left
       assumption
   | succ n ih => -- induction step
       rw [pow_succ] at ha
-      have ha' : a ^ n ∈ I ∨ a ∈ I := by
+      have h : a ^ n ∈ I ∨ a ∈ I := by
         apply Ideal.IsPrime.mem_or_mem
         · assumption
         · assumption
-      rcases ha' with h | h
-      · apply ih
-        assumption
-      · assumption
+      tauto
 
 
-/-- If `a` is nilpotent then `a` is in every prime ideal. -/
-lemma mem_primeIdeal_of_nilpotent
-    {a : R} (ha : IsNilpotent a) {I : Ideal R} (hI : Ideal.IsPrime I) :
+/-- Example 2: If `a` is nilpotent then `a` is in every prime ideal. -/
+lemma mem_primeIdeal_of_nilpotent (R : Type) [CommRing R]
+    (a : R) (ha : IsNilpotent a) (I : Ideal R) (hI : Ideal.IsPrime I) :
     a ∈ I := by
   rw [IsNilpotent] at ha
   obtain ⟨n, hn⟩ := ha
@@ -35,25 +44,18 @@ lemma mem_primeIdeal_of_nilpotent
   · rw [hn]
     apply Ideal.zero_mem
 
-/-- If `I` is a prime ideal, and `a ^ n ∈ I` for some `n`, then `a ∈ I`. -/
-lemma mem_primeIdeal_of_pow_mem'
-    {I : Ideal R} (hI : I.IsPrime) {a : R} {n : ℕ} (ha : a ^ n ∈ I) :
-    a ∈ I := by
-  induction n with
-  | zero => exact mul_one a ▸ I.mul_mem_left a (pow_zero a ▸ ha)
-  | succ n ih => exact (hI.mem_or_mem (pow_succ a n ▸ ha)).elim ih id
 
-omit [Ring R]
 
-variable [CommRing R]
+variable (R : Type) [CommRing R]
 
-lemma IsNilpotent.add {a b : R} (ha : IsNilpotent a) (hb : IsNilpotent b) :
+/-- Sums of nilpotents are nilpotent. -/
+lemma IsNilpotent.add (a b : R) (ha : IsNilpotent a) (hb : IsNilpotent b) :
     IsNilpotent (a + b) := by
   obtain ⟨m, hm⟩ := ha
   obtain ⟨n, hn⟩ := hb
-  use m + n
-  rw [add_pow]
-  apply Finset.sum_eq_zero
+  use m + n -- show (a + b) ^ (m + n) = 0
+  rw [add_pow] -- the binomial theorem
+  apply Finset.sum_eq_zero -- "a sum is zero if all its terms are zero"
   intro i hi
   by_cases hm' : m ≤ i
   · have : i = m + (i - m) := by omega
@@ -61,13 +63,13 @@ lemma IsNilpotent.add {a b : R} (ha : IsNilpotent a) (hb : IsNilpotent b) :
   · have : m + n - i = n + (m - i) := by omega
     simp [this, pow_add, hn]
 
-variable {S : Type*} [CommRing S]
 
-def nilRadical : Submodule S S :=
-{ carrier := {a : S | IsNilpotent a}
+/-- The nilradical of an ideal. [Duplicates an existing definition in mathlib]. -/
+def my_nilradical : Ideal R where
+  carrier := {a : R | IsNilpotent a}
   zero_mem' := by simp
-  add_mem' := IsNilpotent.add
+  add_mem' := by apply IsNilpotent.add
   smul_mem' s t := by
     rintro ⟨n, hn⟩
     use n
-    rw [smul_eq_mul, mul_pow, hn, mul_zero] }
+    rw [smul_eq_mul, mul_pow, hn, mul_zero]
